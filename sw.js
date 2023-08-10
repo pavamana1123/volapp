582016db2165ef463e9e70055ef26cecc815171b
Pavanaprana Dasa <pavamana1123@gmail.com>
2023-08-10 14:23:19 +0530

    let see final

--- sw.js
+++ sw.js
@@ -1,31 +1,32 @@
 self.addEventListener('install', (event) => {
-  self.skipWaiting();
-});
+    self.skipWaiting();
+  });
   
-self.addEventListener('activate', (event) => {
-  self.clients.claim();
-  event.waitUntil(
-    (async () => {
-      if ('navigationPreload' in self.registration) {
-        await self.registration.navigationPreload.enable();
-      }
-    })(),
-  );
-});
-  
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    (async () => {
-      try {
-        const response = await event.preloadResponse;
-        if (response) {
-          return response;
+  self.addEventListener('activate', (event) => {
+    self.clients.claim();
+    event.waitUntil(
+      (async () => {
+        if ('navigationPreload' in self.registration) {
+          await self.registration.navigationPreload.enable();
         }
-        return fetch(event.request);
-      } catch {
-        return new Response('Offline');
-      }
-    })(),
-  );
-});
+      })(),
+    );
+  });
+  
+  
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      (async () => {
+        try {
+          const response = await event.preloadResponse;
+          if (response) {
+            return response;
+          }
+          return fetch(event.request);
+        } catch {
+          return new Response('Offline');
+        }
+      })(),
+    );
+  });
   
