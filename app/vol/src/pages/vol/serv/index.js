dc4d8640d0c9ac86a4c6e5c39d6e77bba4b6fbb9
Pavanaprana Dasa <pavamana1123@gmail.com>
2023-08-10 21:10:04 +0530

    ok slot

--- app/vol/src/pages/vol/serv/index.js
+++ app/vol/src/pages/vol/serv/index.js
@@ -12,16 +12,23 @@ function Serv(props) {
                 <div className='light'>Service</div>
                 <div className='detailFeild'>{details.service}</div>
             </div>
-            <div className='detailsDiv'>
-                <div className='light'>Timings</div>
-                <div className='detailFeild'>{`⏱️ ${details.timings}`}</div>
+            <div className='vol-section'>
+                <div className='detailsDiv'>
+                    <div className='light'>Timings</div>
+                    <div className='detailFeild'>{`⏱️ ${details.timings}`}</div>
+                </div>
+                <vl className="vol-sep"/>
+                <div className='detailsDiv'>
+                    <div className='light'>Your Slot</div>
+                    <div className='detailFeild'>{`⏱️ ${details.availability=="All slots"?details.timings:details.availability}`}</div>
+                </div>
             </div>
             <div className='detailsDiv'>
                 <div className='light'>Co-Ordinator</div>
                 <div className='detailFeild'>{`${details.coordinator}`}</div>
             </div>
             <div className='detailsDiv'>
-                <div className='light'>In-charge</div>
+                <div className='light'>In-charge (SPOC)</div>
                 {<div style={{
                     display: "flex",
                     alignItems: "center"
