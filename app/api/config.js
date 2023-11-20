30fdf3b6c9b3843217f04c3d8868ad03159d51bd
Pavanaprana Dasa <pavamana1123@gmail.com>
2023-11-20 14:02:45 +0530

    vaishee

--- app/api/config.js
+++ app/api/config.js
@@ -1,6 +1,6 @@
 var config = {
         scriptId : "AKfycbxQOS8r9W4tfPi1ps0vuBO9EU7XslOacf2_aJlTSSEQedPeDg9EmF7WOItVshHTF956tQ",
-        sheetID: "1JmtCebw4pXIyP104DABVgUUcU-BSLeLvGRy0ptC7nHQ"
+        sheetID: "1RGRbjuSqC1Hhi6sJ_PPDUCjIO8qvEntATiw-8oXxDyQ"
 }
 
 module.exports = config
