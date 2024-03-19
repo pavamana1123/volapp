6d40681770904a3326e761484bf9b57c1b1db998
Pavanaprana Dasa <pavamana1123@gmail.com>
2024-03-19 13:36:43 +0530

    asd

--- app/api/config.js
+++ app/api/config.js
@@ -1,6 +1,6 @@
 var config = {
         scriptId : "AKfycbxQOS8r9W4tfPi1ps0vuBO9EU7XslOacf2_aJlTSSEQedPeDg9EmF7WOItVshHTF956tQ",
-        sheetID: "1FcTNFNg17LpeONU1CzTMFcenet1u2KD6Ijg9sTbgvUA"
+        sheetID: "1g2o-418dx8k-Sy4j9OjL-c6vmIz97XvXgdkYOV2qJ9E"
 }
 
 module.exports = config
