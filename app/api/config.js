5fccc24346662437c872cfbb58bffefb3b9530a7
Pavana Prana Dasa <pavamana1123@gmail.com>
2025-02-09 22:48:08 +0530

    aaasdas ntd

--- app/api/config.js
+++ app/api/config.js
@@ -1,6 +1,6 @@
 var config = {
         scriptId : "AKfycbxQOS8r9W4tfPi1ps0vuBO9EU7XslOacf2_aJlTSSEQedPeDg9EmF7WOItVshHTF956tQ",
-        sheetID: "1KfZm7UzR5IK7Vsvr0odRIPx_E8iR_CpWYAfBWdNeD-o"
+        sheetID: "1_PTDW677gpBMi-hA5d3t9omzrDge3fCrNFBJGQJunpU"
 }
 
 module.exports = config
