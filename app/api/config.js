2bbe48704833c7c5465650082497ff7e82ff153c
Pavanaprana Dasa <pavamana1123@gmail.com>
2025-08-05 21:52:09 +0530

    sbj

--- app/api/config.js
+++ app/api/config.js
@@ -1,6 +1,6 @@
 var config = {
         scriptId : "AKfycbxQOS8r9W4tfPi1ps0vuBO9EU7XslOacf2_aJlTSSEQedPeDg9EmF7WOItVshHTF956tQ",
-        sheetID: "1m6MU4_QAZJ193P2tpEDir6T2FP9gPyk_8Y2C7-IB7TM"
+        sheetID: "1OmRGQsj11dSV4iIHWRhXs41ciiu05sFtRTuZOGC-1FQ"
 }
 
 module.exports = config
