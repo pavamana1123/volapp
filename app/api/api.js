0663d344f09bc54c2f4bed208c51d8fb2f740ab9
Pavana Prana Dasa <pvpd@iskconmysore.org>
2025-08-06 22:28:15 +0530

    asdasd

--- app/api/api.js
+++ app/api/api.js
@@ -1,63 +1,65 @@
 const os = require('os')
 
 class Cacher {
-    constructor(config){
+    constructor(config) {
         this.scriptId = config.scriptId
         this.sheetID = config.sheetID
         const fs = require('fs')
-        const {google} = require('googleapis')
+        const { google } = require('googleapis')
         var credentials = JSON.parse(fs.readFileSync('credentials.json'))
         var token = JSON.parse(fs.readFileSync('token.json'))
-        const {client_secret, client_id, redirect_uris} = credentials.installed
+        const { client_secret, client_id, redirect_uris } = credentials.installed
 
         const auth = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0])
         auth.setCredentials(token)
 
-        this.client = google.script({version: 'v1', auth})
+        this.client = google.script({ version: 'v1', auth })
 
         this.data = {}
         this.timestamp = null
     }
 
-    start(){
+    start() {
         this.getData()
-        setInterval(this.getData.bind(this), (()=>{
+        setInterval(this.getData.bind(this), (() => {
             if (os.platform() === 'win32') {
-                return 15*60*1000
-              } else {
-                return 5*60*1000
-              }
+                return 15 * 60 * 1000
+            } else {
+                return 5 * 60 * 1000
+            }
         })())
     }
 
-    getData(){
+    getData() {
         var self = this
+        console.log('Initiating data fetch from sheets!')
         this.client.scripts.run({
             resource: {
                 function: "getData",
                 parameters: this.sheetID
             },
             scriptId: this.scriptId,
-        }, function(error, response) {
+        }, function (error, response) {
+            console.log('Received data from sheets!')
             if (error) {
                 console.log("Could not get data:", error)
                 return
             }
 
-            if(!response){
+            if (!response) {
                 console.log("Empty response!")
                 return
-            }else{
-                if(response.error) {
+            } else {
+                if (response.error) {
                     console.log("Error response:", response.error)
                     return
                 }
 
-                if(response.data.error){
+                if (response.data.error) {
                     console.log("Error in data:", response.data.error)
                     return
                 }
-            } 
+            }
             self.data = response.data.response.result
             self.timestamp = new Date()
         })
--- app/api/config.js
+++ app/api/config.js
@@ -1,5 +1,5 @@
 var config = {
-        scriptId : "AKfycbxQOS8r9W4tfPi1ps0vuBO9EU7XslOacf2_aJlTSSEQedPeDg9EmF7WOItVshHTF956tQ",
+        scriptId: "AKfycbxQOS8r9W4tfPi1ps0vuBO9EU7XslOacf2_aJlTSSEQedPeDg9EmF7WOItVshHTF956tQ",
         sheetID: "1m6MU4_QAZJ193P2tpEDir6T2FP9gPyk_8Y2C7-IB7TM"
 }
 
