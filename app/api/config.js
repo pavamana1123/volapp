835e4f89c0b36bd0b81d49b32f251ec4a595459e
Pavana Prana Dasa <pvpd@iskconmysore.org>
2026-03-01 08:38:23 +0530

    asdasd

--- app/api/config.js
+++ app/api/config.js
@@ -1,6 +1,6 @@
 var config = {
         scriptId: "AKfycbxQOS8r9W4tfPi1ps0vuBO9EU7XslOacf2_aJlTSSEQedPeDg9EmF7WOItVshHTF956tQ",
-        sheetID: "1eHPvPYjqWkDKyIgHXaUtj8lZ70jEvIgrij3sRG27L7Y"
+        sheetID: "1nv88HRzPaRCTl_YIVVWc0pwaM0ZFtqLgsDPpqsVX7J4"
 }
 
 module.exports = config
